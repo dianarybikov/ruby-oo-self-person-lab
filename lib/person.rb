958441ee5a79189f5826a9d02b185dca8cecd848
Diana Rybikov <diana.ryb214@gmail.com>
2020-12-22 10:03:54 -0700

    commit tree c6f701c29d7461ee64d625150d27deeaa0472c21

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,31 @@
-# your code goes here
+class Person
+attr_reader :name 
+attr_accessor :bank_account, :happiness, :hygiene
+    
+    def initialize name
+    @name = name
+    @bank_account = 25
+    @happiness = 8 
+    @hygiene = 8
+    end
+    
+    def happiness=(value)
+        if value < 0
+            @happiness = 0
+        elsif value > 10
+            @happiness = 10
+        else
+            @happiness = value
+        end
+    end
+
+    def hygiene=(value)
+        if value < 0
+            @hygiene = 0
+        elsif value > 10
+            @hygiene = 10
+        else
+            @hygiene = value
+        end
+    end
+end
